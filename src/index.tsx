f52f8a9a21d7282c6c8623ad90dd29e7810189ed
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-01-29 01:44:31 +0500

    start from scratch

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import "swiper/scss";
-import "./App.scss";
-
-import Routes from "./Routes";
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root")).render(<Routes />);
