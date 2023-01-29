93e44a2def33dc3408afc1e984f348be1031706c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-01-29 05:34:25 +0500

    hero section done

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,33 @@
+import illustration from "../assets/illustration.svg";
+
 export default function HeroSection() {
   return (
     <div className="hero" id="home">
-      HeroSection
+      <div className="hero__left">
+        <div className="hero__left__slogan">Front End Developer</div>
+        <div className="hero__left__heading">
+          Let's <span>Build</span> You A <span>Digital Identity</span>
+        </div>
+        <div className="hero__left__info">
+          I'm a Web Developer based in Pakistan. Proficent in web and mobile
+          deveopment. I enjoy turning complex problems into simple, beautiful
+          and intuitive solutions. Working at this time for{" "}
+          <a href="https://crazy-beings.web.app/">Crazybeings Studio</a>
+        </div>
+        <button
+          onClick={() => {
+            document
+              .getElementById("contact")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
+          className="hero__left__button"
+        >
+          Lets Talk
+        </button>
+      </div>
+      <div className="hero__right">
+        <img src={illustration} alt="" className="hero__right__img" />
+      </div>
     </div>
   );
 }
