fc79f967ca0e1ff0275d18932aea84ddf1ebef00
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-03-14 13:36:35 +0500

    start from scratch

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import "swiper/scss";
-import "swiper/scss/autoplay";
-import "./styles.scss";
-
-import { Routes } from "./Routes";
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root")).render(<Routes />);
