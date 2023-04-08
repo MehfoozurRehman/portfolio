1b8092c300a88bc2e95b1503e0a26989674c24df
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-09 01:55:06 +0500

    Revert "Revert "start from scratch""
    This reverts commit 478eb978d262de4efa7ba3041df68fb786b0fe63.

--- src/utils/getExperience.tsx
+++
@@ -1,5 +0,0 @@
-export default function getExperience(dateString: string | number | Date) {
-  const diffInMs = Date.now() - new Date(dateString).getTime();
-  const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365);
-  return diffInYears.toPrecision(1);
-}
