df59cef29255bb4989e33a6be112081a15329d1c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-16 07:01:02 +0500

    archive and not found done

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,38 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
 import "./styles.scss";
 
-createRoot(document.getElementById("root")).render(<div>hello</div>);
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Archive from "./screens/Archive";
+import Home from "./screens/Home";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import NotFound from "./screens/NotFound";
+
+function Root() {
+  return (
+    <div className="container">
+      <header></header>
+      <Outlet />
+      <footer></footer>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/archive", element: <Archive /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
