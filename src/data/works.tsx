b024fce310c72bbd80999bf521ebfee895efd52e
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-08-21 13:23:09 +0500

    Update works.tsx

--- src/data/works.tsx
+++ src/data/works.tsx
@@ -1,29 +1,37 @@
-import { aida, dsmeglobal, seevitals } from "../assets";
+import { aida, barry, dsmeglobal } from "../assets";
 
 export const works = [
   {
+    company: "AIDA Pro",
     name: "AIDA Pro",
     tech: ["ReactJS", "ReactNative", "Sass", "Firebase", "Adobe Xd", "Rest"],
     banner: aida,
     info: "A website for startup to help companies and freelancers or jobseekers connect with candidates pre curated for companies according to customized requirements of companies",
     siteLink: "https://aida-pro.web.app/",
-    githubLink: "https://github.com/MehfoozurRehman/Aida-Website",
   },
   {
     right: true,
+    company: "DSME Global",
     name: "DSMEGlobals.com",
     tech: ["ReactJS", "Sass", "Firebase", "Adobe Xd", "Rest"],
     banner: dsmeglobal,
     info: "A website for a startup empowering and helping non technical personal to build creative and intuitive digital identity",
     siteLink: "http://www.dsmeglobal.co/",
-    githubLink: "https://github.com/MehfoozurRehman/dsmeglobal",
   },
   {
-    name: "SeeVitals",
-    tech: ["ReactJS", "Sass", "Firebase", "Adobe Xd", "Rest"],
-    banner: seevitals,
-    info: "A website for startup SeeVitals Technologies is an ambitious project brought to life by 3 friends that have recognized the dire need of technological interventions in the healthcare industry. 2 doctors having worked in numerous healthcare setups from around the world understood that the need to optimize the role of nurses and allied hospital sta…",
-    siteLink: "http://seevitals.com/",
-    githubLink: "https://github.com/MehfoozurRehman/seevitalsweb",
+    name: "Barry ERP",
+    company: "Advanced AFF",
+    tech: [
+      "ReactJS",
+      "Sass",
+      "Express",
+      "NodeJS",
+      "MongoDB",
+      "Adobe Xd",
+      "Rest",
+    ],
+    banner: barry,
+    info: "A dashboard for a food flavor manufacturer to manage their inventory and sales and purchase orders and manage their employees and their salaries and attendance and much more",
+    siteLink: "https://erp.advancedff.com/",
   },
 ];
