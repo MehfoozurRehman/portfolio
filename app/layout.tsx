c070c56f5ae978a0080b183c46ebee9a0d6412dc
mehfoozurrehman <mehfoozijaz786@gmail.com>
2023-12-09 14:15:39 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Header from "@/components/header/Header";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 
