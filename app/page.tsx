e1e68b28c8a4bebdcc1ac83ec2c4e5db6110c3bd
mehfoozurrehman <mehfoozijaz786@gmail.com>
2023-12-09 14:15:42 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
-import Jumbotron from "@/components/Jumbotron";
+import Jumbotron from "@/components/jumbotron/Jumbotron";
 import Testimonials from "@/components/Testimonials";
-import Work from "@/components/Work";
+import Work from "@/components/work/Work";
 
-export default function Home() {
+export default async function Home() {
+  const dataFromGithub = await fetch(
+    "https://api.github.com/users/mehfoozurrehman"
+  ).then((res) => res.json());
   return (
     <>
-      <Jumbotron />
-      <About />
+      <Jumbotron dataFromGithub={dataFromGithub} />
+      <About dataFromGithub={dataFromGithub} />
       <Experience />
       <Work />
       <Testimonials />
