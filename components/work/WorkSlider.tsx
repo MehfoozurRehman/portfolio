a5705582baa5eefbb0ff1c31a03e518c39139553
mehfoozurrehman <mehfoozijaz786@gmail.com>
2023-12-10 19:16:20 +0500

    Update WorkSlider.tsx

--- components/work/WorkSlider.tsx
+++ components/work/WorkSlider.tsx
@@ -1,13 +1,161 @@
 "use client";
 
-import "react-indiana-drag-scroll/dist/style.css";
+import Link from "next/link";
+import ScrollContainer from "react-indiana-drag-scroll";
 
-import { ScrollContainer } from "react-indiana-drag-scroll";
-
-export function WorkSlider({}) {
+export default function WorkSlider() {
   return (
     <ScrollContainer className="work__content__slider">
-      <div className="work__content__slider__entry"></div>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
+      <Link href="/projects/1" className="work__content__slider__entry">
+        <img
+          src="https://cdn.dribbble.com/userupload/7413980/file/original-3ab43399a9768ae7811b5e1c175a6f0c.png?resize=752x752&vertical=center"
+          alt=""
+          className="work__content__slider__entry__img"
+        />
+        <div className="work__content__slider__entry__overlay">
+          <div className="work__content__slider__entry__overlay__title">
+            Project 1
+          </div>
+          <div className="work__content__slider__entry__overlay__desc">
+            Acency website
+          </div>
+        </div>
+      </Link>
     </ScrollContainer>
   );
 }
