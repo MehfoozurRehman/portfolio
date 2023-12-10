bd8b8876322feae0d0a40de02fe5e3440ac4c1e1
mehfoozurrehman <mehfoozijaz786@gmail.com>
2023-12-10 22:03:44 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={nunito.className}>
         {children}
         <SpeedInsights />
