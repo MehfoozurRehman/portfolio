cc26c59be547eba15740d6114fa7782125a16f46
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-06-06 09:54:03 +0500

    Update Landing.tsx

--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -2,11 +2,65 @@ import "../styles/landing.scss";
 
 import { Globe, Layout, Menu, Moon, PhoneOutgoing, Smartphone, Sun, X } from "react-feather";
 import { Link, useLocation } from "react-router";
+import { useEffect, useRef, useState } from "react";
+
+import useTheme from "../hooks/useTheme";
 
 const headerLinks = ["services", "works", "experiences", "testimonials"];
 
+function useToggleMenu() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const [isMenuOpen, setMenuOpen] = useState(true);
+
+  useEffect(() => {
+    const handleResizeWindow = () => {
+      const isSmallScreen = window.innerWidth < 1250;
+      if (isSmallScreen) {
+        setMenuOpen(false);
+      } else {
+        setMenuOpen(true);
+      }
+    };
+
+    handleResizeWindow();
+
+    window.addEventListener("resize", handleResizeWindow);
+
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (window.innerWidth > 1250) return;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return { menuRef: containerRef, isMenuOpen, toggleMenu };
+}
+
 function Header() {
   const location = useLocation();
+
+  const { theme, toggleTheme } = useTheme();
+
+  const { menuRef, isMenuOpen, toggleMenu } = useToggleMenu();
 
   return (
     <div className="landing__container__header">
@@ -14,40 +68,39 @@ function Header() {
         <Link to="/" className="landing__container__header__content__logo">
           Mehfooz ur Rehman
         </Link>
+        {isMenuOpen && (
+          <div ref={menuRef} className="landing__container__header__content__links">
+            {headerLinks.map((link) => {
+              const isActive = location.hash === "#" + link;
 
-        <div className="landing__container__header__content__links">
-          {headerLinks.map((link) => {
-            const isActive = location.hash === "#" + link;
-
-            return (
-              <Link key={link} to={"#" + link} className="landing__container__header__content__links__link">
-                {isActive && <span>{"< "}</span>}
-                {link}
-                {isActive && <span>{" >"}</span>}
-              </Link>
-            );
-          })}
-        </div>
+              return (
+                <Link key={link} to={"#" + link} className="landing__container__header__content__links__link">
+                  {isActive && <span>{"( "}</span>}
+                  {link}
+                  {isActive && <span>{" )"}</span>}
+                </Link>
+              );
+            })}
+          </div>
+        )}
         <div className="landing__container__header__content__actions">
           <div className="landing__container__header__content__actions__entry">
-            <Link to="/contact" className="landing__container__header__content__actions__entry__link">
+            <Link to="tel:+923137178074" className="landing__container__header__content__actions__entry__link">
               +92 313 7178074
             </Link>
-            <PhoneOutgoing size={20} color="currentColor" />
+            <PhoneOutgoing size={18} color="currentColor" />
           </div>
           <div className="landing__container__header__content__actions__entry">
-            <Link to="/contact" className="landing__container__header__content__actions__entry__link">
+            <Link to="#contact" className="landing__container__header__content__actions__entry__contact">
               Contact
             </Link>
           </div>
-          <div className="landing__container__header__content__actions__entry">
-            <Moon size={20} color="currentColor" />
-            <Sun size={20} color="currentColor" />
-          </div>
-          <div className="landing__container__header__content__actions__entry special">
-            <Menu size={20} color="currentColor" />
-            <X size={20} color="currentColor" />
-          </div>
+          <button onClick={() => toggleTheme()} className="landing__container__header__content__actions__entry button">
+            {theme === "light" ? <Moon size={20} color="currentColor" /> : <Sun size={20} color="currentColor" />}
+          </button>
+          <button onClick={() => toggleMenu()} className="landing__container__header__content__actions__entry button special">
+            {isMenuOpen ? <X size={20} color="currentColor" /> : <Menu size={20} color="currentColor" />}
+          </button>
         </div>
       </div>
     </div>
@@ -100,7 +153,7 @@ function About() {
     <div className="landing__container__about">
       <div className="landing__container__about__left">
         {aboutEntries.map((entry) => (
-          <div className="landing__container__about__left__entry">
+          <div key={entry.title} className="landing__container__about__left__entry">
             <div className="landing__container__about__left__entry__icon">{entry.icon}</div>
             <div className="landing__container__about__left__entry__content">
               <div className="landing__container__about__left__entry__content__title">{entry.title}</div>
