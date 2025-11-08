b7df99791f66989e9e9bf641f977199b6d84285d
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-11-08 10:25:50 +0500

    v7

--- app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig } from "@react-router/dev/routes";
-import { nextRoutes } from "rr-next-routes/react-router";
-
-const routes = nextRoutes({ print: "info", folderName: "screens" });
-
-export default routes satisfies RouteConfig;
