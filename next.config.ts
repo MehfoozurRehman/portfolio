3ac01dc85bcf66969ea73d9467279d3613b310c1
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-11-08 10:31:50 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: true,
+  reactCompiler: true,
 };
 
 export default nextConfig;
