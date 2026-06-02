b7ffb04d698ab77bb262207cef2a627358cade69
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2026-06-02 12:21:34 +0500

    Add type declarations for CSS/SCSS imports from external packages

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,13 @@ declare module "*.css" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "*/scss/*" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*/css/*" {
+  const content: Record<string, string>;
+  export default content;
+}
